a91e710c6a6701a894918eb746c8584f28b58524
Thorri <ThorriSnep@pm.me>
2022-10-03 21:56:01 +0200

    added use of macros

--- basics/mowmow/mowmow_lkm.c
+++ basics/mowmow/mowmow_lkm.c
@@ -9,13 +9,13 @@ MODULE_VERSION("0.0.1");
 
 static int __init mowmow_lkm_init(void)
 {
-	printk(KERN_INFO "Mow mow mow, hallo!\n");
+	pr_info("Mow mow mow, hallo!\n");
 	return 0;		/* success */
 }
 
 static void __exit mowmow_lkm_exit(void)
 {
-	printk(KERN_INFO "Mow mow mow, bye!\n");
+	pr_info("Mow mow mow, bye!\n");
 }
 
 module_init(mowmow_lkm_init);
