b5f438debbef105f1063810c940abea588e9e8eb
Thorri <ThorriSnep@pm.me>
2022-10-06 21:33:17 +0200

    fixing typos

--- basics/mowmow/mowmow_lkm.c
+++ basics/mowmow/mowmow_lkm.c
@@ -3,14 +3,14 @@
 #include <linux/module.h>
 
 MODULE_AUTHOR("Thorri Snep");
-MODULE_DESCRIPTION("A Kernal Module that likes to Mows");
+MODULE_DESCRIPTION("A Kernel Module that likes to Mow");
 MODULE_LICENSE("BSD");
-MODULE_VERSION("0.0.1");
+MODULE_VERSION("0.1");
 
 static int __init mowmow_lkm_init(void)
 {
 	pr_info("Mow mow mow, hallo!\n");
-	return 0;		/* success */
+	return 0;
 }
 
 static void __exit mowmow_lkm_exit(void)
